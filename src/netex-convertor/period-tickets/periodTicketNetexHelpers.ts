9481ef45bb0dd62e71735d72acac32e4a609469a
Rob Catton <robmcatton@gmail.com>
2021-04-30 15:30:16 +0100

    Altered private code for lineId inclusion

--- src/netex-convertor/period-tickets/periodTicketNetexHelpers.ts
+++ src/netex-convertor/period-tickets/periodTicketNetexHelpers.ts
@@ -104,10 +104,12 @@ export const getLinesList = (
                 Description: { $t: service.serviceDescription },
                 Url: { $t: currentOperator ? getCleanWebsite(currentOperator.website) : '' },
                 PublicCode: { $t: service.lineName },
-                PrivateCode: {
-                    type: `lineName${service.lineId ?? '_lineId'}`,
-                    $t: `${service.lineName}${service.lineId ?? `_${service.lineId}`}`,
-                },
+                PrivateCode: service.lineId
+                    ? {
+                          type: 'txc:Line@id',
+                          $t: service.lineId,
+                      }
+                    : {},
                 OperatorRef: {
                     version: '1.0',
                     ref: `noc:${operator.nocCode}`,
